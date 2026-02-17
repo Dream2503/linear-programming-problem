a0b7dfcbafa867c5f10b936c4d18da3a6c39e55b
Dream <swapnarajmohanty2503@gmail.com>
2026-02-17 23:25:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@ void test(std::vector<Result>&& res) {
     std::cout << std::endl << std::endl;
 }
 
+void test(LPP&& lpp, const std::string& basis) { std::cout << lpp.dual(basis) << std::endl << std::endl; }
+
 int main() {
     const Variable x("x"), y("y"), z("z"), x1("x1"), x2("x2"), x3("x3");
 
@@ -33,65 +35,76 @@ int main() {
              {
                  x + y <= 4,
                  x - y <= 2,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, x1 + x2 + 3 * x3,
              {
                  3 * x1 + 2 * x2 + x3 <= 3,
                  2 * x1 + x2 + 2 * x3 <= 2,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     test(LPP(Optimization::MINIMIZE, x1 - 3 * x2 + 2 * x3,
              {
                  3 * x1 - x2 + 2 * x3 <= 7,
                  -2 * x1 + 4 * x2 <= 12,
                  -4 * x1 + 3 * x2 + 8 * x3 <= 10,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     test(LPP(Optimization::MAXIMIZE, 2 * x + y,
              {
                  4 * x + 3 * y <= 12,
                  4 * x + y <= 8,
                  4 * x - y <= 8,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, 2 * x + y,
              {
                  x - y <= 10,
                  2 * x - y <= 40,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, 3 * x + 2 * y,
              {
                  x - y <= 1,
                  3 * x - 2 * y <= 6,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, x1 + 2 * x2 + x3,
              {
                  2 * x1 + x2 - x3 >= -2,
                  -2 * x1 + x2 - 5 * x3 <= 6,
                  4 * x1 + x2 + x3 <= 6,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     // Big M Method
     test(LPP(Optimization::MAXIMIZE, -4 * x1 - x2,
              {
                  3 * x1 + x2 == 3,
                  4 * x1 + 3 * x2 >= 6,
                  x1 + 2 * x2 <= 3,
-             }));
+             },
+             {x1 >= 0, x2 >= 0}));
     test(LPP(Optimization::MAXIMIZE, -x - y,
              {
                  3 * x + 2 * y >= 30,
                  -2 * x + 3 * y <= -30,
                  x + y <= 5,
-             }));
+             },
+             {x >= 0, y >= 0}));
     // Two Phase Method
     test(LPP(Optimization::MAXIMIZE, -4 * x - y,
              {
                  3 * x + y == 3,
                  4 * x + 3 * y >= 6,
                  x + 2 * y <= 3,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, 3 * x1 + 2 * x2 + x3,
              {
                  -3 * x1 + 2 * x2 + 2 * x3 == 8,
                  -3 * x1 + 4 * x2 + x3 == 7,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     // BFS
     test(basic_feasible_solutions({
         x1 + 2 * x2 + x3 == 4,
@@ -105,31 +118,36 @@ int main() {
              {
                  x1 + 2 * x3 == 0,
                  x2 + x3 == 1,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     test(LPP(Optimization::MAXIMIZE, 2 * x1 + 3 * x2 + 10 * x3,
              {
                  x1 - 2 * x3 == 0,
                  x2 + x3 == 1,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     test(LPP(Optimization::MAXIMIZE, 2 * x + y,
              {
                  4 * x + 3 * y <= 12,
                  4 * x + y <= 8,
                  4 * x - y <= 8,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(LPP(Optimization::MAXIMIZE, 5 * x + 3 * y,
              {
                  x + y <= 2,
                  5 * x + 2 * y <= 10,
                  3 * x + 8 * y <= 12,
-             }));
+             },
+             {x >= 0, y >= 0}));
     // Surprise Test
     test(LPP(Optimization::MAXIMIZE, 2 * x + 3 * y,
              {
                  x + 2 * y >= 2,
                  3 * x + y >= 3,
                  4 * x + 3 * y <= 6,
-             }));
+             },
+             {x >= 0, y >= 0}));
     test(basic_feasible_solutions({
         x + 2 * y - z == 3,
         3 * x - y + 2 * z == 4,
@@ -140,12 +158,51 @@ int main() {
                  2 * x1 + x2 - x3 >= -2,
                  -2 * x1 + x2 - 5 * x3 <= 6,
                  4 * x1 + x2 + x3 <= 6,
-             }));
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}));
     test(LPP(Optimization::MAXIMIZE, 2 * x + 3 * y,
              {
                  x + 2 * y <= 4,
                  x + y == 3,
-             }));
-
+             },
+             {x >= 0, y >= 0}));
+    test(LPP(Optimization::MAXIMIZE, 3 * x + 2 * y,
+             {
+                 4 * x - 3 * y <= 10,
+                 x + 2 * y <= 5,
+                 3 * x - 5 * y >= 15,
+             },
+             {x >= 0, y >= 0}),
+         "w");
+    test(LPP(Optimization::MINIMIZE, 5 * x1 + 4 * x2 - 3 * x3,
+             {
+                 x1 + x2 + x3 >= 5,
+                 2 * x1 + 3 * x2 - 5 * x3 <= 4,
+                 x1 + 2 * x2 - 3 * x3 <= 6,
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}),
+         "w");
+    test(LPP(Optimization::MAXIMIZE, x + 2 * y,
+             {
+                 3 * x + 2 * y <= 4,
+                 2 * x - 5 * y == 10,
+             },
+             {x >= 0, y >= 0}),
+         "w");
+    test(LPP(Optimization::MINIMIZE, 3 * x1 + 4 * x2 + 7 * x3,
+             {
+                 x1 + 2 * x2 - 5 * x3 >= 4,
+                 2 * x1 + 5 * x2 - x3 == 7,
+                 2 * x1 + 3 * x2 - x3 <= -8,
+             },
+             {x1 >= 0, x2 >= 0, x3 >= 0}),
+         "w");
+    test(LPP(Optimization::MAXIMIZE, 3 * x + 2 * y,
+             {
+                 x + y <= 5,
+                 2 * x + 3 * y >= 4,
+                 x - y <= 2,
+             },
+             {LPP::unrestrict(x), y >= 0}), "w");
     return 0;
 }
